ba977cb50be79135a137b0ed7a37df0aedfbd18a
AlejandroAriasSL <alejandroasilva02@gmail>
2025-09-13 00:37:29 +0200

    Test: It should retrieve instace of MockInjection

--- test/inject.test.ts
+++ test/inject.test.ts
@@ -13,8 +13,18 @@ describe("@Injectable decorator", () => {
   
   it("It should inject the class within the container upon module import", async() => {
 
-    const { MockInjection } = await import("./mocks/MockInjection")
+    const { MockInjection } = await import("./mocks/MockInjection");
     expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(MockInjection);
+  })
+
+  it("It should retrieve instace of MockInjection" , async() => {
+    spy = vi.spyOn(container, "get");
+    
+    const { MockInjection } = await import("./mocks/MockInjection");
+    const instance = container.get(MockInjection)
     expect(spy).toHaveBeenCalledWith(MockInjection)
+
+    expect(instance).toBeInstanceOf(MockInjection);
   })
 })
