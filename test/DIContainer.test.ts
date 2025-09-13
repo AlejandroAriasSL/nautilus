588f1f546c477715bea8edc9e0c2a6e84a0d68f5
AlejandroAriasSL <alejandroasilva02@gmail>
2025-09-13 23:31:11 +0200

    Test: DIContainer should clean previous instances

--- test/DIContainer.test.ts
+++ test/DIContainer.test.ts
@@ -2,8 +2,11 @@ import DIContainer from "@src/DIContainer"
 import Injectable from "@src/Injectable"
 
 
-describe("DIContainer test", () => {
+describe("DIContainer", () => {
     let container: DIContainer;
+
+    @Injectable
+    class Foo{}
 
     beforeEach(() =>  {
         container = DIContainer.getInstance()
@@ -11,9 +14,16 @@ describe("DIContainer test", () => {
 
     it("should register classes correctly", () => {
 
-        @Injectable
-        class Foo{}
-
         expect(container.hasClass(Foo)).toBe(true);
+    })
+
+    it("should clean previous instances", ()=> {
+
+        expect(container.hasClass(Foo)).toBe(true)
+
+        DIContainer.resetInstance();
+        container = DIContainer.getInstance();
+
+        expect(container.hasClass(Foo)).toBe(false)
     })
 })
