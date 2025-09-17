c8c2afeb2ac1d85fa8512e732c883401d67dec0b
AlejandroAriasSL <alejandroasilva02@gmail>
2025-09-17 02:21:26 +0200

    Feature: Load styles dinamically (WIP)

--- src/core/TemplateLoader.ts
+++ src/core/TemplateLoader.ts
@@ -1,77 +1,108 @@
-import ComponentRegistry, { ChildMetadata } from "@src/registries/ComponentRegistry";
+import ComponentRegistry, {
+  ChildMetadata,
+} from "@src/registries/ComponentRegistry";
 
-interface TemplateRecord{
-    templateUrl: string,
-    componentClass: Function
+interface TemplateRecord {
+  templateUrl: string;
+  styleUrl?: string;
+  componentClass: Function;
 }
 
-export default class TemplateLoader{
+export default class TemplateLoader {
+  private static parser = new DOMParser();
+  private static templates = new Map<string, TemplateRecord>();
 
-    private static parser = new DOMParser();
-    private static templates = new Map<string, TemplateRecord>;
+  public static save(path: string, templateRecord: TemplateRecord): void {
+    this.templates.set(path, templateRecord);
+  }
 
-    public static save(path: string, templateUrl: string, componentClass: Function) : void {
-        this.templates.set(path, {templateUrl, componentClass});
-    }
+  public static async load(path: string): Promise<void> {
+    const record = this.templates.get(path);
+    const root = document.body.querySelector<HTMLElement>("#root");
 
-    public static async load(path: string) : Promise<void> {
-        const record = this.templates.get(path);
-        const root = document.body.querySelector<HTMLElement>("#root");
+    if (!record || !root) return;
 
-        if (!record || !root) return;
+    const { templateUrl, componentClass, styleUrl } = record;
+    const template = await this.getTemplate(templateUrl).catch((error) =>
+      console.error(error)
+    );
 
-        const {templateUrl, componentClass} = record   
-        root.innerHTML = ""
-        const template = await this.getTemplate(templateUrl).catch(error => console.error(error));
+    if (!template) return;
+    
+    const rootElement = template.querySelector<HTMLTemplateElement>("template");
+    const clone = rootElement?.content.cloneNode(true);
+    root.replaceChildren(clone!);
 
-        if(!template) return;
-        const rootElement = template.querySelector<HTMLTemplateElement>("template");
-        const clone = rootElement?.content.cloneNode(true);
-        root.appendChild(clone!);
+    this.loadStyles(styleUrl);
 
-        console.log("main template loaded")
+    console.log("main template loaded");
 
-        const children = ComponentRegistry.getChildren(componentClass); 
-        console.log(children)
-        if (children.length === 0) return;
+    const children = ComponentRegistry.getChildren(componentClass);
+    console.log(children);
 
-        this.renderChildren(root, children);
+    this.renderChildren(root, children);
 
-        console.log(root)
-    } 
+    console.log(root);
+  }
 
-    private static async renderChildren(root : HTMLElement, children: ChildMetadata[]) : Promise<void>{
-        for( const child of children){
-            const slot = root.querySelector(child.slot);
-            if (!slot) continue;
+  private static loadStyles(styleUrl: string | undefined): void {
 
-            const childDocument = await this.getTemplate(child.templateUrl).catch(err => {
-                console.error(err);
-                return null;
-            });
-            if (!childDocument) continue;
+    const head = document.head;
+    
+    head.querySelectorAll('link[data-style="true"]').forEach(link => link.remove());
+    
+    if (!styleUrl) return;
 
-            const childTemplate = childDocument.querySelector<HTMLTemplateElement>("template");
-            if (!childTemplate) continue;
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = styleUrl;
+    link.setAttribute("data-style", "true");
 
-            const childClone = childTemplate.content.cloneNode(true);
-            slot.appendChild(childClone);
-            console.log("children appended to slot")
+    head.appendChild(link);
+  }
+
+  private static async renderChildren(
+    root: HTMLElement,
+    children: ChildMetadata[]
+  ): Promise<void> {
+    if (children.length === 0) return;
+
+    for (const child of children) {
+      const slot = root.querySelector(child.slot);
+      if (!slot) continue;
+
+      const childDocument = await this.getTemplate(child.templateUrl).catch(
+        (err) => {
+          console.error(err);
+          return null;
         }
+      );
+      if (!childDocument) continue;
+
+      const childTemplate =
+        childDocument.querySelector<HTMLTemplateElement>("template");
+      if (!childTemplate) continue;
+
+      const childClone = childTemplate.content.cloneNode(true);
+      slot.appendChild(childClone);
+      console.log("children appended to slot");
+    }
+  }
+
+  private static async getTemplate(url: string): Promise<Document> {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(
+        `Template loader failed to fetch template: ${url} (status: ${response.status})`
+      );
     }
 
-    private static async getTemplate(url : string) : Promise<Document> {
-        const response = await fetch(url);
+    const rawText = await response.text();
+    return this.parseToHtml(rawText);
+  }
 
-        if (!response.ok){
-            throw new Error(`Template loader failed to fetch template: ${url} (status: ${response.status})`)
-        }
-
-        const rawText = await response.text();
-        return this.parseToHtml(rawText);
-    }
-
-    private static parseToHtml(rawText: string) : Document {
-        return this.parser.parseFromString(rawText, "text/html")
-    }
+  private static parseToHtml(rawText: string): Document {
+    return this.parser.parseFromString(rawText, "text/html");
+  }
 }
--- src/decorators/Component.ts
+++ src/decorators/Component.ts
@@ -3,12 +3,21 @@ import Injectable from "./Injectable";
 
 interface ComponentConfig{
     templateUrl: string,
+    styleUrl?: string
     path: string,
 }
 
 export default function Component(componentConfig: ComponentConfig){
-    return function(method : any, context: ClassDecoratorContext){
-        TemplateLoader.save(componentConfig.path, componentConfig.templateUrl, method);
-        Injectable(method)
+    return function(target : any, context: ClassDecoratorContext){
+        const {templateUrl, styleUrl, path} = componentConfig
+        
+        const templateLoader = {
+            templateUrl: templateUrl,
+            styleUrl: styleUrl,
+            componentClass: target  
+        }
+
+        TemplateLoader.save(path, templateLoader);
+        Injectable(target)
     }
 }
